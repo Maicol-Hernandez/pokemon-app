4a2015737f86bfdaf75960d3531b2a9b6952ef0d
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-02-06 08:54:04 -0500

    Updates Angular application dependencies
    Adds support for HttpClient, animations, and PrimeNG.
    
    Improves application structure and performance.

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,13 +1,16 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    providePrimeNG({ theme: {} }),
+    provideZoneChangeDetection({ eventCoalescing: true }),
   ],
 };
